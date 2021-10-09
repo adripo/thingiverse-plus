11cb05745505852b7315b4a05c1e5e1361bff1d3
adripo <26493496+adripo@users.noreply.github.com>
2021-10-09 16:36:39 +0200

    updated meta

---
+++ ThingiversePlus.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Thingiverse Plus
+// @namespace    https://thingiverse.com/
+// @version      0.2
+// @description  try to take over the world!
+// @author       adripo
+// @icon         https://www.thingiverse.com/favicon.ico
+// @match        https://www.thingiverse.com/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
