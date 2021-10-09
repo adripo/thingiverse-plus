61c02211ec590e6c38a93bee94afc3a0514586ad
adripo <26493496+adripo@users.noreply.github.com>
2021-10-09 16:41:05 +0200

    updated meta

--- ThingiversePlus.user.js
+++ ThingiversePlus.user.js
@@ -2,9 +2,13 @@
 // @name         Thingiverse Plus
 // @namespace    https://thingiverse.com/
 // @version      0.2
-// @description  try to take over the world!
+// @description  Thingiverse with improved functionality
 // @author       adripo
+// @homepage     https://github.com/adripo/thingiverse-plus
 // @icon         https://www.thingiverse.com/favicon.ico
+// @updateURL    https://raw.githubusercontent.com/adripo/thingiverse-plus/main/ThingiversePlus.meta.js
+// @downloadURL  https://raw.githubusercontent.com/adripo/thingiverse-plus/main/ThingiversePlus.user.js
+// @supportURL   https://github.com/adripo/thingiverse-plus/issues
 // @match        https://www.thingiverse.com/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @grant        GM_addStyle
