8a6245236e5b4b18fa1bacbaee3c95a215c0a1df
QuBIT <workflow@github.com>
2023-07-18 17:24:45 +0000

    Updated minified script and meta

--- ThingiversePlus.meta.js
+++ ThingiversePlus.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Thingiverse Plus
 // @namespace    https://thingiverse.com/
-// @version      2.0.2
+// @version      2.0.3
 // @description  Thingiverse with extra features
 // @author       adripo
 // @homepage     https://github.com/adripo/thingiverse-plus
