d91f10356103dcdd79a1cad609c91af23636109c
adripo <26493496+adripo@users.noreply.github.com>
2024-02-02 19:44:31 +0100

    chore: version bump

--- src/ThingiversePlus.src.user.js
+++ src/ThingiversePlus.src.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Thingiverse Plus
 // @namespace    https://thingiverse.com/
-// @version      2.0.3
+// @version      3.0.0
 // @description  Thingiverse with extra features
 // @author       adripo
 // @homepage     https://github.com/adripo/thingiverse-plus
